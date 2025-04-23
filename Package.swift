19e9ad8cc72f0dfe643d6a4df434d88254dc226b
AgamBhullar4 <agam@hawcx.com>
2025-04-23 13:53:57 -0500

    Swift Package Manager support

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HawcxFramework",
-            url: "https://github.com/hawcx/hawcx_ios_sdk/releases/download/1.0.1/HawcxFramework.xcframework.zip",
+            url: "https://github.com/user-attachments/files/19874294/HawcxFramework.xcframework.zip",
             checksum: "a3116363c0bfb4dc52eb74131b66014a619c494d2a19970092bf78d10cbb4caf"
         )
     ]
