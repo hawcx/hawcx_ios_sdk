f5c2c58f95847934c1c46349a7e547f74dc762e5
AgamBhullar4 <agam@hawcx.com>
2025-04-25 17:57:17 -0500

    checksum update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "HawcxFramework",
             url: "https://github.com/hawcx/hawcx_ios_sdk/releases/download/1.0.1/HawcxFramework.xcframework.zip",
-            checksum: "1ac1d93235a0551c46740ea8e102c9da758d03dad93bef88861203021347f15d"
+            checksum: "a8d7279575ba3e65e3f061513a59149c6ddfe22309a996e7819136de92613e6a"
         )
     ]
 )
