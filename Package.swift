dadc56d1e57d098c139a1e82e2db50a28027b590
AgamBhullar4 <agam@hawcx.com>
2025-06-11 00:54:43 -0500

    Release version 4.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HawcxFramework",
-            url: "https://github.com/hawcx/hawcx_ios_sdk/releases/download/4.0.1/HawcxFramework.xcframework.zip",
+            url: "https://github.com/hawcx/hawcx_ios_sdk/releases/download/4.0.0/HawcxFramework.xcframework.zip",
             checksum: "74a23567d992ecee4b50b041d1e2ee5e606f9af06d9b33255267f18e54a0cdfb"
         )
     ]
